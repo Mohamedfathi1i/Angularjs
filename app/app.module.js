58c50229e219af4a93a452994ea08ef0e2d3b305
Mohamed Fathi <MY_NAME@example.com>
2023-09-04 18:36:12 +0300

    v3

--- app/app.module.js
+++ app/app.module.js
@@ -1,10 +1,14 @@
 'use strict';
 
-// Define the `phonecatApp` module
 var app = angular.module('myApp', []);
+
+
 app.controller('firstController', appModule)
 
+
 appModule.$inject = ['$scope'];
+
+
 
 function appModule($scope) {
     $scope.firstName = "John";
